63471387020189eab87a67c9ea2d5424ad970788
leonardo <leonardo_trifiletti@yahoo.it>
2024-02-11 18:42:40 +0100

    cors refactoring

--- index.js
+++ index.js
@@ -1,44 +1,49 @@
-import express from "express"
-import morgan from "morgan"
-import mongoose from "mongoose"
-import authenticationRouter from "./routes/authentication.js"
-import trainingCardsRouter from "./routes/trainingCards.js"
-import cors from "cors"
-import dotenv from "dotenv"
-dotenv.config()
-import { requireAuth } from "./library/cryptingPass.js"
+import express from "express";
+import morgan from "morgan";
+import mongoose from "mongoose";
+import authenticationRouter from "./routes/authentication.js";
+import trainingCardsRouter from "./routes/trainingCards.js";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+import { requireAuth } from "./library/cryptingPass.js";
 
-const{MONGO_URI} = process.env;
+const { MONGO_URI } = process.env;
 
 // My server
 
-const app = express()
+const app = express();
 
 //Middleware
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
-app.use(cors({origin: "*"}))
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 
- //routes 
-app.use('/authentication', authenticationRouter);
+//routes
+app.use("/authentication", authenticationRouter);
 
- //Authentication middleware
- app.use(requireAuth); 
- 
+//Authentication middleware
+app.use(requireAuth);
+
 //User routes
-app.use("/trainingcards", trainingCardsRouter) 
+app.use("/trainingcards", trainingCardsRouter);
 
 //Mongoose connect
 
-mongoose.connect(MONGO_URI)
-.then(()=> {
-    console.log("Successfully connected to MongoDB")
-    app.listen(3000,()=> {
-        console.log("Server listen on the port")
-    })
-})
-.catch(err => console.error(err));
-
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Successfully connected to MongoDB");
+    app.listen(3000, () => {
+      console.log("Server listen on the port");
+    });
+  })
+  .catch((err) => console.error(err));
 
 export default app;
