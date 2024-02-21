ff4b495f55880e6dddc8ff77dd1c66c85dd919c9
3ifo <leonardo_trifiletti@yahoo.it>
2024-02-21 17:38:34 +0100

    add vercel front-end link

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["final-project-front-beta.vercel.app"],
     credentials: true,
   })
 ); 
